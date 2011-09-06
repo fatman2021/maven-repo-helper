af694fa1f183490fbb6f33e995edf77d6b9121d0
Ludovic Claude <ludovic.claude@laposte.net>
2011-09-06 22:36:54 +0000

    * Deprecate mh_installjar -i option and replace it with -j to avoid conflicts   with -i/--ignore-rules option used elsewhere. * Review all options passed to the mh_* scripts, in particular --set-version   for mh_installjar (Closes: #640730)

---
+++ src/main/share/jh_maven_repo_helper.pm
@@ -0,0 +1,13 @@
+#!/usr/bin/perl                                                                                                                
+use warnings;
+use strict;
+use Debian::Debhelper::Dh_Lib;
+
+# To use with javahelper
+# dh $@ --with javahelper --with jh_mavenrepohelper
+
+insert_after("jh_depends", "mh_installpoms");
+insert_after("mh_installpoms", "mh_linkjars --skip-clean-poms");
+insert_before("dh_clean", "mh_clean");
+
+1;
