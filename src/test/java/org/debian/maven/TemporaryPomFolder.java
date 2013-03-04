1e1415859b7505a5a9fbfc46be97cd99468ec414
Thomas Koch <thomas@koch.ro>
2013-03-04 21:20:57 +0100

    removed unused imports

--- src/test/java/org/debian/maven/TemporaryPomFolder.java
+++ src/test/java/org/debian/maven/TemporaryPomFolder.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.LineNumberReader;
 import java.io.Reader;
 import java.net.URISyntaxException;
 import java.net.URL;
