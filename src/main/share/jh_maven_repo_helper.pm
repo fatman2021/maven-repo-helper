70ebe9cc29ae3f1dcd31548a87d1a401a4b84caf
Emmanuel Bourg <ebourg@apache.org>
2013-05-23 15:29:52 +0200

    Fixed the usage of the debhelper functions in jh_maven_repo_helper.pm. Thanks to Wolodja Wentland for the fix (Closes: #709446)

--- src/main/share/jh_maven_repo_helper.pm
+++ src/main/share/jh_maven_repo_helper.pm
@@ -7,7 +7,8 @@ use Debian::Debhelper::Dh_Lib;
 # dh $@ --with javahelper --with jh_mavenrepohelper
 
 insert_after("jh_depends", "mh_installpoms");
-insert_after("mh_installpoms", "mh_linkjars --skip-clean-poms");
+insert_after("mh_installpoms", "mh_linkjars");
+add_command_options("mh_linkjars", "--skip-clean-poms");
 insert_before("dh_clean", "mh_clean");
 
 1;
