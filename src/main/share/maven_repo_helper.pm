3b34a86cac06dc30a26238f56a57842de366fa1b
Emmanuel Bourg <ebourg@apache.org>
2015-06-10 12:47:16 +0200

    Insert mh_install after dh_install

--- src/main/share/maven_repo_helper.pm
+++ src/main/share/maven_repo_helper.pm
@@ -5,7 +5,7 @@ use Debian::Debhelper::Dh_Lib;
 
 # dh $@ --with maven_repo_helper
 
-insert_before("dh_compress", "mh_install");
+insert_after("dh_install", "mh_install");
 insert_before("dh_clean", "mh_clean");
 
 1;
